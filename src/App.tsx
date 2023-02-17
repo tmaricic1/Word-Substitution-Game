2ab2a868a5d2509b3e87612a59e663d62de88950
Tony <31423416+tmaricic1@users.noreply.github.com>
2023-02-17 00:02:57 -0500

    preliminary style (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="Title">Word Substitution Game</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Please choose the correct word to complete the sentence. Have fun!
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="sentence-box">
+        <p>The United Kingdom ____ more books than any other country.</p>
+      </div>
+      <div className="answers-box">
+        <div className="answer">makes</div>
+        <div className="answer">publishes</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
